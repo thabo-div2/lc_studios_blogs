138d04ef47985febe67114e426b9bf8ae7411c52
thabo-div2 <thabosetsubi3@gmail.com>
2021-10-27 16:09:57 +0200

    timestamp commit

--- src/views/ViewPost.vue
+++ src/views/ViewPost.vue
@@ -1,14 +1,15 @@
 <template>
 	<div>
 		<div v-if="documents">
-			<div v-for="doc in documents" :key="doc">
+			<div v-for="doc in formattedDocuments" :key="doc">
 				<h3>{{ doc.title }}</h3>
 				<img :src="doc.coverUrl" />
-				<p>Made by: {{ doc.userName }}</p>
+				<p>Author: {{ doc.userName }}</p>
 				<p>{{ doc.content }}</p>
 				<div v-for="tag in doc.tags" :key="tag">
 					<p># {{ tag }} ,</p>
 				</div>
+				<p>{{ doc.createdAt }}</p>
 			</div>
 		</div>
 	</div>
@@ -16,12 +17,22 @@
 
 <script>
 import getCollection from "@/composables/getCollection";
+import { formatDistanceToNow } from "date-fns";
 
 export default {
 	setup() {
 		const { error, documents } = getCollection("posts");
 
-		return { error, documents };
+		const formattedDocuments = computed(() => {
+			if (documents.value) {
+				return documents.value.map((doc) => {
+					let time = formatDistanceToNow(doc.createdAt.toDate());
+					return { ...doc, createdAt: time };
+				});
+			}
+		});
+
+		return { error, documents, formattedDocuments };
 	},
 };
 </script>
