43dfb0cfb14f9691c16fa164cae2fcb4a8855f5b
thabo-div2 <thabosetsubi3@gmail.com>
2021-10-28 11:25:27 +0200

    init

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,33 @@
 <template>
-  <nav>
-    <div>
-      <p>hey there... name</p>
-      <p class="email">Currently logged in as... email</p>
-    </div>
-    <button @click="handleClick">Logout</button>
-  </nav>
+	<nav>
+		<div v-if="user">
+			<div>
+				<p>hey there... {{ user.displayName }}</p>
+				<p class="email">Currently logged in as... {{ user.email }}</p>
+			</div>
+			<button @click="handleClick">Logout</button>
+		</div>
+	</nav>
 </template>
 
 <script>
 import useLogout from "../composables/useLogout";
+import getUsers from "../composables/getUsers";
 
 export default {
-  setup() {
-    const { logout, error } = useLogout();
+	setup() {
+		const { user } = getUsers();
+		const { logout, error } = useLogout();
 
-    const handleClick = async () => {
-      await logout();
-      if (!error.value) {
-        console.log("user logged out");
-      }
-    };
+		const handleClick = async () => {
+			await logout();
+			if (!error.value) {
+				console.log("user logged out");
+			}
+		};
 
-    return { handleClick };
-  },
+		return { handleClick, user };
+	},
 };
 </script>
 
