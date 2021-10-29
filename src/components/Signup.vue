fddfb1c68b662f7ceb87a01bd15f164b52e0a179
Aneeqahj <aneeqahjones2@gmail.com>
2021-10-29 14:32:03 +0200

    Changes to the Sign in form

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,36 +1,36 @@
 <template>
-	<form @submit.prevent="handleSubmit">
-		<input
-			class="inputs"
-			type="text"
-			required
-			placeholder="name"
-			v-model="displayName"
-		/>
-		<input
-			class="inputs"
-			type="text"
-			required
-			placeholder="surname"
-			v-model="surname"
-		/>
-		<input
-			class="inputs"
-			type="email"
-			required
-			placeholder="email"
-			v-model="email"
-		/>
-		<input
-			class="inputs"
-			type="password"
-			required
-			placeholder="password"
-			v-model="password"
-		/>
-		<div class="error">{{ error }}</div>
-		<button class="signup_btn">Sign up</button>
-	</form>
+  <form @submit.prevent="handleSubmit">
+    <input
+      class="inputs"
+      type="text"
+      required
+      placeholder="name"
+      v-model="displayName"
+    />
+    <input
+      class="inputs"
+      type="text"
+      required
+      placeholder="surname"
+      v-model="surname"
+    />
+    <input
+      class="inputs"
+      type="email"
+      required
+      placeholder="email"
+      v-model="email"
+    />
+    <input
+      class="inputs"
+      type="password"
+      required
+      placeholder="password"
+      v-model="password"
+    />
+    <div class="error">{{ error }}</div>
+    <button class="signup_btn">Sign up</button>
+  </form>
 </template>
 
 <script>
@@ -39,60 +39,59 @@ import useSignup from "../composables/useSignup";
 import { useRouter } from "vue-router";
 
 export default {
-	setup() {
-		const { error, signup } = useSignup();
-		const router = useRouter();
+  setup() {
+    const { error, signup } = useSignup();
+    const router = useRouter();
 
-		// refs
-		const displayName = ref("");
-		const surname = ref("");
-		const email = ref("");
-		const password = ref("");
+    // refs
+    const displayName = ref("");
+    const surname = ref("");
+    const email = ref("");
+    const password = ref("");
 
-		const handleSubmit = async () => {
-			await signup(email.value, password.value, displayName.value);
-			console.log("user signed up");
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value);
+      console.log("user signed up");
 
-			router.push("/posts");
-		};
+      router.push("/posts");
+    };
 
-		return { displayName, surname, email, password, handleSubmit, error };
-	},
+    return { displayName, surname, email, password, handleSubmit, error };
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
 .signup_subtitle {
-	color: #e3ad1b;
-	/* -webkit-text-stroke: 0.5px black; */
-	/* -webkit-font-smoothing: antialiased; */
-	font-size: 28px;
-	font-family: "Barlow", sans-serif;
+  color: #e3ad1b;
+  /* -webkit-text-stroke: 0.5px black; */
+  /* -webkit-font-smoothing: antialiased; */
+  font-size: 28px;
+  font-family: "Barlow", sans-serif;
 }
 .signup_btn {
-	background: #7c8ebf;
-	border: none;
-	width: 35%;
-	padding: 9px 0 9px 0;
-	border-radius: 15px;
-	font-family: "Barlow", sans-serif;
-	font-size: 14px;
-	font-weight: 600;
-	color: white;
+  border: none;
+  width: 35%;
+  padding: 9px 0 9px 0;
+  border-radius: 15px;
+  font-family: "Barlow", sans-serif;
+  font-size: 14px;
+  font-weight: 600;
+  margin: 15px auto;
+  color: white;
+  background-color: #7c8ebf;
 }
 .signup_btn:hover {
-	cursor: pointer;
+  cursor: pointer;
 }
 .login_hover {
-	color: #e3ad1b;
-	cursor: pointer;
-	font-family: "Barlow", sans-serif;
-	font-weight: 600;
-	font-size: 19px;
+  color: #e3ad1b;
+  cursor: pointer;
+  font-family: "Barlow", sans-serif;
+  font-weight: 600;
+  font-size: 19px;
 }
 .login_hover_content {
-	font-family: "Barlow", sans-serif;
-	font-weight: 500;
-	font-size: 19px;
+  font-family: "Barlow", sans-serif;
 }
 </style>
