f2394ef20d6d2a176c925237ef33f3f647014270
thabo-div2 <thabosetsubi3@gmail.com>
2021-11-03 09:21:48 +0200

    update comments v2

--- src/components/CommentsWindow.vue
+++ src/components/CommentsWindow.vue
@@ -5,9 +5,11 @@
 		</div>
 		<div v-if="documents" class="messages" ref="messages">
 			<div v-for="doc in formattedDocuments" :key="doc.id">
-				<span>{{ doc.createdAt }}</span>
-				<span>{{ doc.name }}</span>
-				<span>{{ doc.comment }}</span>
+				<div v-if="doc.postId === props.doc">
+					<span>{{ doc.createdAt }}</span>
+					<span>{{ doc.name }}</span>
+					<span>{{ doc.comment }}</span>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -19,7 +21,8 @@ import { formatDistanceToNow } from "date-fns";
 import { computed, ref } from "@vue/reactivity";
 import { onUpdated } from "@vue/runtime-core";
 export default {
-	setup() {
+	props: ["doc"],
+	setup(props) {
 		const { error, documents } = getCollection("comments");
 
 		const formattedDocuments = computed(() => {
@@ -27,6 +30,7 @@ export default {
 				console.log(documents.value);
 				return documents.value.map((doc) => {
 					const time = formatDistanceToNow(doc.createdAt.toDate());
+
 					return { ...doc, createdAt: time };
 				});
 			}
@@ -39,7 +43,7 @@ export default {
 			messages.value.scrollTop = messages.value.scrollHeight;
 		});
 
-		return { error, documents, formattedDocuments, messages };
+		return { error, documents, formattedDocuments, messages, props };
 	},
 };
 </script>
