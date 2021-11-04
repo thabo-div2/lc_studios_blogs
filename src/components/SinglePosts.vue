a6cc65b4da53cd2d07faeb676a489369b6b34a36
thabo-div2 <thabosetsubi3@gmail.com>
2021-11-04 08:47:00 +0200

    update modal

--- src/components/SinglePosts.vue
+++ src/components/SinglePosts.vue
@@ -5,7 +5,10 @@
 			<span class="post_time">
 				<p class="time">{{ doc.createdAt }} ago</p>
 			</span>
-			<router-link :to="{ name: 'SinglePost', params: { id: doc.id } }"  class="post_title_container">
+			<router-link
+				:to="{ name: 'SinglePost', params: { id: doc.id } }"
+				class="post_title_container"
+			>
 				<div class="post_heading">
 					<h3 class="post_title style">{{ doc.title }}</h3>
 				</div>
@@ -35,40 +38,40 @@ export default {
 </script>
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Barlow:wght@300;700&family=Mukta:wght@300&display=swap');
-	.post_container {
-		width: 90vw;
-		height: 100vh;
-		margin: 0 0 80px 0;
-		background: black;
-	}
-	.post_info {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	.post_time {
-		height: 100px;
-		width: 100px;
-		background-color: #F8F8F8;
-		border-radius: 50%;
-		display: flex;
-		overflow: hidden;
-		justify-content: center;
-		align-items: center;
-		margin: -50px 0 0 0;
-    	border: solid 1px black;
-	}
-	.time {
-		width: 90%;
-		font-family: 'Mukta', sans-serif;
-		color: black;
-	}
-	.post_title {
-		font-weight: 900;
-	}
-	.style {
-		font-family: 'Mukta', sans-serif;
-		color: #FFFFFF;
-	}
+@import url("https://fonts.googleapis.com/css2?family=Barlow:wght@300;700&family=Mukta:wght@300&display=swap");
+.post_container {
+	width: 90vw;
+	height: 100vh;
+	margin: 0 0 80px 0;
+	background: black;
+}
+.post_info {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.post_time {
+	height: 100px;
+	width: 100px;
+	background-color: #f8f8f8;
+	border-radius: 50%;
+	display: flex;
+	overflow: hidden;
+	justify-content: center;
+	align-items: center;
+	margin: -50px 0 0 0;
+	border: solid 1px black;
+}
+.time {
+	width: 90%;
+	font-family: "Mukta", sans-serif;
+	color: black;
+}
+.post_title {
+	font-weight: 900;
+}
+.style {
+	font-family: "Mukta", sans-serif;
+	color: #ffffff;
+}
 </style>
---
+++ src/components/UpdateModal.vue
@@ -0,0 +1,19 @@
+<template>
+	<transition name="modal-animation">
+		<div v-show="modalActive" class="modal">
+			<transition name="modal-animation-inner">
+				<div v-show="modalActive" class="modal-inner">
+					<slot />
+				</div>
+			</transition>
+		</div>
+	</transition>
+</template>
+
+<script>
+export default {
+	props: ["modalActive"],
+};
+</script>
+
+<style></style>
